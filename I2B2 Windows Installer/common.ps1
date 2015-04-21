97627d9150aebc60da32bfd768a86fb4bc28a550
JP_Hahn <PCFHahn@hotmail.com>
2015-04-21 09:39:11 -0500

    Created createCert function to generate a self-signed certificate with hostname as alias.  Automated $_SHRINE_IP, $_I2B2_IP_DEFAULT, and $_KEYSTORE_ALIAS variables in common. Commented MySQL variables and added $_SHRINE_MSSQL_HOST variable.

--- I2B2 Windows Installer/common.ps1
+++ I2B2 Windows Installer/common.ps1
@@ -6,13 +6,13 @@ $_SHRINE_VERSION = "1.18.2"
 
 $_I2B2_DOMAIN_ID = "i2b2demo"
 
-$_SHRINE_IP = "localhost"
+$_SHRINE_IP = ((Invoke-WebRequest "http://checkip.dyndns.com").ParsedHtml.Body.innerHTML -split ': ')[1]
 $_SHRINE_MSSQL_SERVER = "localhost"
 
 $_NEXUS_URL_BASE = "http://repo.open.med.harvard.edu/nexus/content/groups/public/net/shrine"
 $_SHRINE_SVN_URL_BASE = "https://open.med.harvard.edu/svn/shrine/releases/$_SHRINE_VERSION"
 
-$_I2B2_IP_DEFAULT = "localhost:9090"
+$_I2B2_IP_DEFAULT = $($_SHRINE_IP.Trim() + ":9090")
 
 $_I2B2_PM_IP = $_I2B2_IP_DEFAULT
 $_I2B2_ONT_IP = $_I2B2_IP_DEFAULT
@@ -42,7 +42,7 @@ $_SHRINE_TOMCAT_SERVER_CONF = "$_SHRINE_TOMCAT_HOME\conf\server.xml"
 $_SHRINE_TOMCAT_APP_CONF = "$_SHRINE_TOMCAT_HOME\conf\Catalina\localhost\shrine.xml"
 
 #The human-readable name of the Shrine node being installed.  This string will appear in the web UI as a label on a source of query results.
-$_SHRINE_NODE_NAME = "Harvard Affiliated Teaching Hospital"
+$_SHRINE_NODE_NAME = "Washington University School of Medicine"
 
 #The cryptographic keystore file used by Shrine.
 $_KEYSTORE_FILE = "$_SHRINE_HOME\shrine.keystore"
@@ -51,28 +51,31 @@ $_KEYSTORE_FILE = "$_SHRINE_HOME\shrine.keystore"
 $_KEYSTORE_PASSWORD = "changeit"
 
 #Human-readable name for the cryptographic certificate generated for this Shrine node.
-$_KEYSTORE_ALIAS = $_SHRINE_IP
+$_KEYSTORE_ALIAS = $env:COMPUTERNAME
 
 #Human-readable name for the cryptographic certificate generated for this Shrine node.
 $_KEYSTORE_HUMAN = $_SHRINE_NODE_NAME
 
 #City where the node resides; will be included in generated cryptographic certificate.
-$_KEYSTORE_CITY = "Boston"
+$_KEYSTORE_CITY = "St. Louis"
 
 #State where the node resides; will be included in generated cryptographic certificate.
-$_KEYSTORE_STATE = "MA"
+$_KEYSTORE_STATE = "MO"
 
 #Country where the node resides; will be included in generated cryptographic certificate.
 $_KEYSTORE_COUNTRY = "US"
 
 #The host at which MySQL is accessible.  MySQL is needed by Shrine for logging purposes.
-$_SHRINE_MYSQL_HOST = "localhost"
+#$_SHRINE_MYSQL_HOST = "localhost"
 
 #The MySQL user Shrine should log in as.
-$_SHRINE_MYSQL_USER = "shrine"
+#$_SHRINE_MYSQL_USER = "shrine"
 
 #The password for the MySQL user that Shrine should log in as.
-$_SHRINE_MYSQL_PASSWORD = "demouser"
+#$_SHRINE_MYSQL_PASSWORD = "demouser"
+
+#The host at which MSSQL is accessible.
+$_SHRINE_MSSQL_HOST = "localhost"
 
 #The database name for the MSSQL server instance
 $_SHRINE_MSSQL_DB = "shrine"
@@ -93,5 +96,5 @@ $_SHRINE_ADAPTER_I2B2_PASSWORD = "demouser"
 $_SHRINE_ADAPTER_I2B2_PROJECT = "Demo"
 
 #The i2b2 domain Shrine should use when making queries to an i2b2 hive.
-#NB: This needs to match I2B2_DOMAIN_ID:(
+#NB: This needs to match I2B2_DOMAIN_ID:
 $_SHRINE_ADAPTER_I2B2_DOMAIN = "i2b2demo"
